6b0cd67fd29dd7299090a1bea3dd281e944e9414
AndreiKastsiukovich <andreikastsiukovich@gmail.com>
2023-04-24 18:12:22 +0300

    Commit-1

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,9 +1,17 @@
 const initState = {
     themeId: 1,
+
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionType): typeof state=> { // fix any
     switch (action.type) {
+        case "SET_THEME_ID":{
+            return {
+                ...state,
+                themeId:action.id
+            }
+
+        }
         // дописать
 
         default:
@@ -11,4 +19,9 @@ export const themeReducer = (state = initState, action: any): any => { // fix an
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = {
+    type:'SET_THEME_ID',
+    id:number
+}
+
+export const changeThemeId = (id: number): ActionType => ({ type: 'SET_THEME_ID', id }) // fix any
